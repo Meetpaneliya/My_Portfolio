e4476c86fd82303938b9859de5917ae45cccf66a
Meetpaneliya <meetpaneliya032@gmail.com>
2025-01-25 16:00:50 +0530

    save2

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Meetpaneliya",
   resume:
-    "https://drive.google.com/file/d/1WSzVqMKwSj48jKHxVdD9z9AhMsfu-SHW/view?usp=drive_link",
+    "https://drive.google.com/file/d/1ffk8Pzkq7ryv5J5BdFcSYsQnU40Ek8hS/view?usp=drive_link",
   linkedin: "https://www.linkedin.com/in/meet-paneliya-61779222a/",
   insta: "https://www.instagram.com/meet_paneliya/?hl=en",
   facebook: "",
